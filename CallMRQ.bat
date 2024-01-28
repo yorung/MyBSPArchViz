8b2063ae1d394b91d3596bd9c92e040740099e30
yorung <yorung@gmail.com>
2024-01-28 22:47:27 +0900

    Panorama MRQ test

---
+++ CallMRQ.bat
@@ -0,0 +1,2 @@
+"E:\Program Files\Epic Games\UE_5.2\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/PanoramaMRQ/MPQ_1024x512.MPQ_1024x512" -windowed -resx=1280 -resy=720 -log -notexturestreaming
+
