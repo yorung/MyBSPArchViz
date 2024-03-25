f38187a4f2ccb14f7d5be278dc2dda92a7a4e987
yorung <yorung@gmail.com>
2024-03-25 20:39:32 +0900

    batch for rendering

--- CallMRQ.bat
+++ CallMRQ.bat
@@ -1,2 +1,3 @@
-"E:\Program Files\Epic Games\UE_5.2\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/PanoramaMRQ/MPQ_1024x512.MPQ_1024x512" -windowed -resx=1280 -resy=720 -log -notexturestreaming
-
+SET EDITORCMD="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
+%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/PanoramaMRQ/MPQ_1024x512.MPQ_1024x512" -windowed -resx=1280 -resy=720 -log -notexturestreaming
+%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/510V3/MPQ_510V3.MPQ_510V3" -windowed -resx=1280 -resy=720 -log -notexturestreaming
