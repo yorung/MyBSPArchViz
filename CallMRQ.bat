fbc5b4205cbfaea4db692b66b550e8b811cf3bbc
yorung <yorung@gmail.com>
2024-03-25 21:27:47 +0900

    Rendering movies

--- CallMRQ.bat
+++ CallMRQ.bat
@@ -1,3 +1,4 @@
 SET EDITORCMD="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
 %EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/PanoramaMRQ/MPQ_1024x512.MPQ_1024x512" -windowed -resx=1280 -resy=720 -log -notexturestreaming
-%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/510V3/MPQ_510V3.MPQ_510V3" -windowed -resx=1280 -resy=720 -log -notexturestreaming
+%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/510V3/MPQ_510V3_Still.MPQ_510V3_Still" -windowed -resx=1280 -resy=720 -log -notexturestreaming
+%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/510V3/MPQ_510V3_Movie.MPQ_510V3_Movie" -windowed -resx=1280 -resy=720 -log -notexturestreaming
