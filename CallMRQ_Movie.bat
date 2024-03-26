e554bd6f7dec6cf424766676cc962747e6531c31
yorung <yorung@gmail.com>
2024-03-26 20:55:06 +0900

    Split render batch

---
+++ CallMRQ_Movie.bat
@@ -0,0 +1,2 @@
+SET EDITORCMD="C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\UnrealEditor-Cmd.exe"
+%EDITORCMD% "E:\Project\MyBSPArchViz\MyBSPArchViz.uproject" -game -MoviePipelineConfig="/Game/510V3/MPQ_510V3_Movie.MPQ_510V3_Movie" -windowed -resx=1280 -resy=720 -log -notexturestreaming
